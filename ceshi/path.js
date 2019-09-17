63361083ae958a8e43ba2b576d4d4548b4c22be7
wangteng <wangteng@yingmob.com>
2019-09-17 16:39:42 +0800

    test

---
+++ ceshi/path.js
@@ -0,0 +1,5 @@
+const path = require('path');
+
+// console.log(path.basename('/dailynote/ceshi'));
+// console.log(path.basename('/dailynote/ceshi/path.js'));
+// console.log(path.basename('/dailynote/ceshi/path.js','.js'));
